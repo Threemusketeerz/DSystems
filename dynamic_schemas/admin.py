044ca37395755c5683f247a1494c6505aea7fce9
Bjarke <work.bjarke@gmail.com>
2017-12-12 14:48:39 +0100

    Added filtering, save_on_top.

--- dynamic_schemas/admin.py
+++ dynamic_schemas/admin.py
@@ -5,13 +5,27 @@ from .models import Schema, SchemaQuestion, SchemaHelpUrl
 
 
 class QuestionInline(admin.StackedInline):
+    fieldsets = [         
+        (None, {
+            'fields': ('text',),
+            }
+        ),
+        (None, {
+            'classes': ('radio',),
+            'fields': ('is_response_bool', 'is_editable',),
+            }
+        ),
+    ]
     model = SchemaQuestion
     extra = 1
 
 
 class SchemaAdmin(admin.ModelAdmin):
+    save_on_top = True
+    list_display = ('name', 'is_active',)
+    list_filter = ('is_active',)
     fieldsets = [
-        (None, {'fields': ['name', 'help_field']}),
+        (None, {'fields': ['name', 'help_field', 'is_active']}),
     ]
     inlines = [QuestionInline]
 
