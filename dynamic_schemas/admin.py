ca4283742b471be7d50cb362798ca25c9d6ff9e2
Bjarke <work.bjarke@gmail.com>
2017-12-13 11:50:05 +0100

    It's now impossible to save if schema is locked. Visual of denial repr is lacking

--- dynamic_schemas/admin.py
+++ dynamic_schemas/admin.py
@@ -1,7 +1,19 @@
+from django import forms
 from django.contrib import admin
+
 from .models import Schema, SchemaQuestion, SchemaHelpUrl
 
 # Register your models here.
+class SchemaAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs) 
+        # __import__('ipdb').set_trace()
+        if kwargs['instance'].is_locked:
+            self.fields['is_locked'].widget = forms.HiddenInput()
+    
+    class Meta:
+        model = Schema
+        fields = ('__all__')
 
 
 class QuestionInline(admin.StackedInline):
@@ -17,17 +29,37 @@ class QuestionInline(admin.StackedInline):
         ),
     ]
     model = SchemaQuestion
-    extra = 1
+    extra = 0
+
 
 
 class SchemaAdmin(admin.ModelAdmin):
+    form = SchemaAdminForm
     save_on_top = True
-    list_display = ('name', 'is_active',)
-    list_filter = ('is_active',)
+    list_display = ('name', 'is_active', 'is_locked')
+    list_filter = ('is_active', 'is_locked')
     fieldsets = [
-        (None, {'fields': ['name', 'help_field', 'is_active']}),
+        (None, {'fields': ['name', 'help_field', 'is_active', 'is_locked']}),
     ]
     inlines = [QuestionInline]
+
+
+    # def schema_lock(self, val_to_check_for):
+        # fields = self.fieldsets[0][1]['fields']
+        # if val_to_check_for in fields:
+            # __import__('ipdb').set_trace()
+            # # This is a bit unpythonish, but does the job, since the lists are
+            # # static (luckily).
+            # self.fieldsets[0][1]['fields'].pop()
+            # self.inlines.pop()
+
+    # def save_model(self, request, obj, form, change):
+        # if obj.is_locked:
+            # self.schema_lock('is_locked')
+
+        # if not obj.is_locked:
+            # self.inlines.append(QuestionInline)
+        # super().save_model(request, obj, form, change)
 
 
 admin.site.register(Schema, SchemaAdmin)
